a47945ef7551016d5d3dec8920ae7ed21f0c5a79
TechFreel <xpzm14@gmail.com>
2021-11-09 10:30:59 +0900

    no message

--- Studyjsp/src/MemberJoin.java
+++ Studyjsp/src/MemberJoin.java
@@ -11,24 +11,25 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class MemberJoin
  */
 @WebServlet("/MemberJoin.do")
+@WebServlet("/memberJoin.do")
 public class MemberJoin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberJoin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) 
-		throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//1. 클라이언트의 요청을 받는다 : 매개변수를 가져온다 ▶ HttpServletRequest
+	public MemberJoin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ① 클라이언트의 요청을 받는다 : 매개변수를 가져온다. ▶ HttpServletRequest
 		request.setCharacterEncoding("utf-8");
 		String name = request.getParameter("name");
 		String id = request.getParameter("id");
@@ -36,11 +37,6 @@ public class MemberJoin extends HttpServlet {
 		int age = Integer.parseInt(request.getParameter("age"));
 		String addr = request.getParameter("addr");
 		
-		System.out.println("이름 : "+ name);
-		System.out.println("아이디 : "+ id);
-		System.out.println("비밀버호 : "+ pw);
-		System.out.println("나이 : "+ age);
-		System.out.println("주소 : "+ addr);
 	}
 
 	/**
--- studyjsp/src/MemberJoin.java
+++ studyjsp/src/MemberJoin.java
@@ -1,4 +1,5 @@
 
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberJoin
  */
+@WebServlet("/MemberJoin.do")
 @WebServlet("/memberJoin.do")
 public class MemberJoin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -35,29 +37,20 @@ public class MemberJoin extends HttpServlet {
 		int age = Integer.parseInt(request.getParameter("age"));
 		String addr = request.getParameter("addr");
 		
-		System.out.println("이름 : " + name);
-		System.out.println("아이디 : " + id);
-		System.out.println("비빌번호 : " + pw);
-		System.out.println("나이 : " + age);
-		System.out.println("주소 : " + addr);
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
