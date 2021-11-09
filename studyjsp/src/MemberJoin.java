f67a305f7c1c12b886f5ca2bf782efcd87f77259
chesed96 <kangeunhye55@gmail.com>
2021-11-09 16:01:09 +0900

    2021.11.09 Web_01.FirstProject

--- studyjsp/src/MemberJoin.java
+++ studyjsp/src/MemberJoin.java
@@ -1,10 +1,14 @@
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hanul.member.MemberDTO;
 
 /**
  * Servlet implementation class MemberJoin
@@ -40,6 +44,15 @@ public class MemberJoin extends HttpServlet {
 		System.out.println("비빌번호 : " + pw);
 		System.out.println("나이 : " + age);
 		System.out.println("주소 : " + addr);
+		
+		//② 비지니스 로직	: DAO를 통해서 DB와 연동하고 결과를 리턴받는 작업
+		MemberDTO dto = new MemberDTO(name, id, pw, age, addr);
+		//DB연동: MemberDAO.java → memberInsert(dto); ▶ 생략
+		
+		//③ 프리젠테이션 로직 : 리턴받은 결과를 클라이언트에게 응답 → *.html, &.jsp
+		request.setAttribute("dto", dto);	//연결객체 생성
+		RequestDispatcher rd = request.getRequestDispatcher("result.jsp");	//페이지 호출
+		rd.forward(request, response);	//페이지 전환
 	}
 
 	/**
