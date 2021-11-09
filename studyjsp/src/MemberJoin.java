839bc351634baf5235074145f03ddd4cfa7cc37c
ju-eun <wndms9060@gmail.com>
2021-11-09 15:01:50 +0900

    sadasdsadasdsa

--- studyjsp/src/MemberJoin.java
+++ studyjsp/src/MemberJoin.java
@@ -1,5 +1,8 @@
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> keh
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/memberJoin.do")
 public class MemberJoin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+<<<<<<< HEAD
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +32,24 @@ public class MemberJoin extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException {
 		//① 클라이언트의 요청을 받는다 : 매개변수를 가져온다 ▶ HttpServletRequest
+=======
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MemberJoin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ① 클라이언트의 요청을 받는다 : 매개변수를 가져온다. ▶ HttpServletRequest
+>>>>>>> keh
 		request.setCharacterEncoding("utf-8");
 		String name = request.getParameter("name");
 		String id = request.getParameter("id");
@@ -37,23 +59,43 @@ public class MemberJoin extends HttpServlet {
 		
 		System.out.println("이름 : " + name);
 		System.out.println("아이디 : " + id);
+<<<<<<< HEAD
 		System.out.println("비밀번호 : " + pw);
+=======
+		System.out.println("비빌번호 : " + pw);
+>>>>>>> keh
 		System.out.println("나이 : " + age);
 		System.out.println("주소 : " + addr);
 	}
 
 	/**
+<<<<<<< HEAD
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+=======
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+>>>>>>> keh
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
+<<<<<<< HEAD
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+=======
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+>>>>>>> keh
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
