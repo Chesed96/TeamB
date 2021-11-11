391fbafb9160ac898371df7636d10f3c8f3606c1
chesed96 <kangeunhye55@gmail.com>
2021-11-11 17:54:38 +0900

    no message

--- studyview/WebContent/6.Test_emee/script/main.js
+++
@@ -1,6 +0,0 @@
-const MOBILE = document.querySelector("#btn-menu");
-const VPWRAP = document.querySelector(".vp-wrap");
-
-MOBILE.addEventListener("click", function(){
-	VPWRAP.style.display="block";
-});
