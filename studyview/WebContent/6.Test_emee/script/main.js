6756e4fe779be23cbf465f6770553b0f552ad3e5
TechFreel <xpzm14@gmail.com>
2021-11-11 15:17:56 +0900

    no message

---
+++ studyview/WebContent/6.Test_emee/script/main.js
@@ -0,0 +1,6 @@
+const MOBILE = document.querySelector("#btn-menu");
+const VPWRAP = document.querySelector(".vp-wrap");
+
+MOBILE.addEventListener("click", function(){
+	VPWRAP.style.display="block";
+});
